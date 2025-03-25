ee2f8cd0f01adfb1521c9098ca0235949678a9d9
86198 <1939942064@qq.com>
2025-03-25 15:09:55 +0800

    sshi

--- top/apps.py
+++ top/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HhhConfig(AppConfig):
+class TopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "polls"
+    name = "top"
